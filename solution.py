f967e57203e8bb4938ca2a0f94b4a8a3bd52cb24
Denis <mistrny@gmail.com>
2023-04-12 22:49:29 +0300

    attempt 1

--- solution.py
+++ solution.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
+
+chat_id = 264956206  # Ваш chat ID, не меняйте название переменной
+alpha = 0.06
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+def solution(x, y) -> bool:
+    # Проверяем равенство дисперсий выборок
+    var_x = np.var(x)
+    var_y = np.var(x)
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    # Выполняем t-тест
+    t_stat, p_val = ttest_ind(x, x, equal_var=var_x == var_y)
+
+    # Сравниваем p-значение с уровнем значимости
+    return p_val < alpha  # Не отклоняем нулевую гипотезу
