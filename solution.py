fa8efd4e117b0425b67365e70c077ac61d3ccd34
Denis <mistrny@gmail.com>
2023-04-12 23:10:45 +0300

    attempt2

--- solution.py
+++ solution.py
@@ -9,10 +9,10 @@ alpha = 0.06
 def solution(x, y) -> bool:
     # Проверяем равенство дисперсий выборок
     var_x = np.var(x)
-    var_y = np.var(x)
+    var_y = np.var(y)
 
     # Выполняем t-тест
-    t_stat, p_val = ttest_ind(x, x, equal_var=var_x == var_y)
+    t_stat, p_val = ttest_ind(x, y, equal_var=var_x == var_y)
 
     # Сравниваем p-значение с уровнем значимости
     return p_val < alpha  # Не отклоняем нулевую гипотезу
